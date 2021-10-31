664339a3106e83f1631bbbe867f673b41d394673
Alexey Basalov <6y6jlb@gmail.com>
2021-10-31 14:21:17 +0300

    Updates

---
+++ precache-manifest.965aa89cc006f7ea41466a92e07a8ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdb2875c9c576ba93c861adbf933a2e",
+    "url": "/react-ws-chat/index.html"
+  },
+  {
+    "revision": "b7382cac4517c87e5ebb",
+    "url": "/react-ws-chat/static/css/main.b82d0c7a.chunk.css"
+  },
+  {
+    "revision": "5a74222ec35139cee004",
+    "url": "/react-ws-chat/static/js/2.d193f582.chunk.js"
+  },
+  {
+    "revision": "7437750837ebc9c195a2",
+    "url": "/react-ws-chat/static/js/3.3e4bf5ab.chunk.js"
+  },
+  {
+    "revision": "b7382cac4517c87e5ebb",
+    "url": "/react-ws-chat/static/js/main.98934730.chunk.js"
+  },
+  {
+    "revision": "be45b8a6a3da4dd90ea6",
+    "url": "/react-ws-chat/static/js/runtime-main.ba8bfba9.js"
+  }
+]);
