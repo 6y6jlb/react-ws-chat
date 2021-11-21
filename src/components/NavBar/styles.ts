c3c9ff1aa7fec856b7d9ef86c60423433f97a1c9
Alexey Basalov <6y6jlb@gmail.com>
2021-11-21 14:54:08 +0300

    bug fix

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,18 +1,21 @@
 import { makeStyles } from '@mui/styles';
 
-
 export const useStyles = makeStyles({
     root:{
         justifyContent:"space-around",
-        display:"grid",
-        gridTemplateRows:'1fr 5fr 1fr',
+        display:"flex",
+        columnGap:30,
+        flexWrap:'wrap',
+        rowGap:4,
+        margin:'8px 0'
     },
     title: {
         display:"grid",
         gridTemplateRows:'1fr 1fr',
         justifyItems:"flex-end",
+
     },
     onlineCounter:{
         color:"grey"
-    }
+    },
 });
---
+++ src/utils/const.ts
@@ -0,0 +1,1 @@
+export const MOBIL = 520;
